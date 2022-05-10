fd1b0abc7eb03114f17d5bb2a1cbb1185ef58a98
Brandon <bcharris713@gmail.com>
2022-05-10 13:09:05 -0500

    commit tree e9c9279b3cf66d0de29b27c9b40f357d00763348

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(gifts) {
+    const newArray = [];
+    for (let i = 0; i < gifts.length; i++) {
+        newArray.push(`Thank you, ${gifts[i]}, for the wonderful surprise gift!`);
+    }
+    return newArray;
+}
+
+function countDown(number) {  
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
